3619d2711d084ec6e0962d17b46b3c951ce37748
copsgit <github@copsking.com>
2023-01-10 17:56:47 -0500

    marketplace sdk

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -7,26 +7,84 @@ const StateContext = createContext();
 const contractAddress = import.meta.env.VITE_APP_CONTRACT_ADDRESS;
 
 export const StateContextProvider = ({children}) => {
-    const {contract} = useContract(contractAddress)
+    const {contract} = useContract(contractAddress, "marketplace")
     // const {mutateAsync: createCampaign} = useContractWrite(contract, 'createCampaign');
 
     const address = useAddress();
     const connect = useMetamask();
     const disconnect = useDisconnect();
 
-    const getCampaigns = async () => {
-        const campaigns = await contract.call('getCampaigns');
-        console.log(campaigns)
-        return campaigns.map((campaign, i) => ({
-            owner: campaign.owner,
-            title: campaign.title,
-            description: campaign.description,
-            target: ethers.utils.formatEther(campaign.target.toString()),
-            deadline: campaign.deadline.toNumber(),
-            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
-            image: campaign.image,
+    const purchaseNft = async (id, quantity) => {
+        const tx = await contract.buyoutListing(id, quantity);
+    }
+
+    const getActiveListings = async () => {
+        const listings = await contract.call('getActiveListings');
+        console.log(listings)
+        return listings.map((listing, i) => ({
+            owner: listing.owner,
+            title: listing.title,
+            description: listing.description,
+            price: ethers.utils.formatEther(listing.price.toString()),
+            quantity: listing.quantity.toNumber(),
+            image: listing.image,
             pId: i
         }));
+    }
+
+    const getAllListings = async () => {
+        const listings = await contract.call('getAllListings');
+        console.log(listings)
+        return listings.map((listing, i) => ({
+            owner: listing.owner,
+            title: listing.title,
+            description: listing.description,
+            price: ethers.utils.formatEther(listing.price.toString()),
+            quantity: listing.quantity.toNumber(),
+            image: listing.image,
+            pId: i
+        }));
+    }
+
+    const getAllOffersOfOneListing = async (id) => {
+        const offers = await contract.getOffers(id);
+        console.log(offers)
+        return offers.map((offer, i) => ({
+            owner: offer.owner,
+            price: ethers.utils.formatEther(offer.price.toString()),
+            quantity: offer.quantity.toNumber(),
+            pId: i
+        }));
+    }
+
+    const makeOffer = async (id, quantity, price) => {
+        const tx = await contract.makeOffer(id, quantity, price);
+        return tx;
+    }
+
+    const makeListing = async (assetContractAddress, currencyContractAddress, buyoutPricePerToken, listingDurationInSeconds, tokenId) => {
+        const listing = {
+            // address of the NFT contract the asset you want to list is on
+            assetContractAddress,
+            // token ID of the asset you want to list
+            tokenId,
+            // when should the listing open up for offers
+            startTimestamp: new Date(),
+            // how long the listing will be open for
+            listingDurationInSeconds,
+            // how many of the asset you want to list
+            quantity: 1,
+            // address of the currency contract that will be used to pay for the listing
+            currencyContractAddress,
+            // how much the asset will be sold for
+            buyoutPricePerToken,
+        }
+
+        const tx = await contract.direct.createListing(listing);
+        const receipt = tx.receipt; // the transaction receipt
+        const listingId = tx.id; // the id of the newly created listing
+
+        return tx;
     }
 
 
@@ -37,7 +95,12 @@ export const StateContextProvider = ({children}) => {
                 contract,
                 connect,
                 disconnect,
-                getCampaigns,
+                purchaseNft,
+                getActiveListings,
+                makeOffer,
+                makeListing,
+                getAllOffersOfOneListing,
+                getAllListings
             }}
         >
             {children}
