ac6d7f3b20e0b0f626d548077ad24ec2cf0d812e
copsgit <github@copsking.com>
2023-01-14 02:05:00 -0500

    collection ui

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -7,10 +7,6 @@ import {useStateContext} from "../context";
 import {MediaRenderer} from "@thirdweb-dev/react";
 
 const Collection = () => {
-    // const data = useSelector(state => state.item.data[0]);
-    // const data1 = useSelector(state => state.item.data);
-    // const items = [...data1, ...data1, ...data1, ...data1, ...data1]
-
     const {alchemy} = useStateContext();
     const [info, setInfo] = useState(null);
     const contractAddress = window.location.pathname.split("/")[2];
@@ -28,7 +24,6 @@ const Collection = () => {
         <div className="
                         flex flex-col justify-center bg-[#e6e7e9] items-center w-11/12 mx-auto mt-3
             rounded-3xl shadow-2xl shadow-[#ed3c00] overflow-y-auto
-
         ">
 
             <div className="
@@ -44,14 +39,14 @@ const Collection = () => {
                 />
             </div>
             <div className="
-                flex flex-row justify-between items-center
-                relative bottom-1/2
+                flex flex-row justify-start items-center
+                relative top-[-70px]
             ">
                 <CollectionInfo data={info?.nfts[0]} items={info?.nfts?.length}/>
             </div>
 
             <div className="
-                w-11/12 mt-3 mb-5
+                w-11/12 mb-5 relative top-[-70px]
             ">
                 <CardsList type={"detail"} items={info?.nfts}/>
             </div>
