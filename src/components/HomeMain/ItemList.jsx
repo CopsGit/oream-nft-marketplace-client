7ecdb10dec7d9a4ec93bb30ec361e828791e1334
copsgit <github@copsking.com>
2023-01-17 18:29:08 -0500

    STYLE: Update collection card height

--- src/components/HomeMain/ItemList.jsx
+++ src/components/HomeMain/ItemList.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {eth} from "../../assets";
 import HomeCollectionCard from "../Cards/HomeCollectionCard";
 import {useSelector} from "react-redux";
-import {Grid} from "@mui/material";
+import {Box, Grid} from "@mui/material";
 
 const ItemList = ({items}) => {
 
     return (
-        <div className="
-            flex flex-col h-3/5 w-full px-5 py-3
+        <Box className="
+            flex flex-col w-full px-5 py-3
         ">
             <p className="
                 text-xl font-bold text-center text-[#fe7700] mb-3
@@ -17,21 +17,21 @@ const ItemList = ({items}) => {
             </p>
             <Grid
                 container
-                spacing={1}
+                spacing={2}
                 className="
                     h-full w-full
                 "
             >
                 {items.map((item, index) => (
                     <Grid
-                        item xs={12} sm={4} md={3} lg={2.4} key={index}
+                        item xs={12} sm={4} md={3} lg={2.4} key={index} height={300}
                     >
                         <HomeCollectionCard item={item} type={"collection"}/>
                     </Grid>
                 ))}
 
             </Grid>
-        </div>
+        </Box>
     );
 };
 
