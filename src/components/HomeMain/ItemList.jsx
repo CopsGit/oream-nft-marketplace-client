b9d76d400207197ca109f612181a590a4b2f2049
copsgit <github@copsking.com>
2023-01-17 18:57:54 -0500

    STYLE: Update home collections responsive

--- src/components/HomeMain/ItemList.jsx
+++ src/components/HomeMain/ItemList.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {eth} from "../../assets";
 import HomeCollectionCard from "../Cards/HomeCollectionCard";
 import {useSelector} from "react-redux";
-import {Box, Grid} from "@mui/material";
+import {Grid} from "@mui/material";
 
 const ItemList = ({items}) => {
 
     return (
-        <Box className="
-            flex flex-col w-full px-5 py-3
+        <div className="
+            flex flex-col h-3/5 w-full px-5 py-3
         ">
             <p className="
                 text-xl font-bold text-center text-[#fe7700] mb-3
@@ -17,21 +17,31 @@ const ItemList = ({items}) => {
             </p>
             <Grid
                 container
-                spacing={2}
+                spacing={1}
                 className="
-                    h-full w-full
+                    h-full w-full flex flex-row justify-around
                 "
             >
                 {items.map((item, index) => (
                     <Grid
-                        item xs={12} sm={4} md={3} lg={2.4} key={index} height={300}
+                        item xs={12} sm={4} md={2.4} lg={2.4} key={index}
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: {
+                                xs: '100%',
+                                sm: 170,
+                                md: 300,
+                            }
+                        }}
                     >
                         <HomeCollectionCard item={item} type={"collection"}/>
                     </Grid>
                 ))}
 
             </Grid>
-        </Box>
+        </div>
     );
 };
 
